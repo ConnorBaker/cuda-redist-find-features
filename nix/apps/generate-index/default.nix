da08910df426da0497b783518637da36e4d61970
Connor Baker <connor.baker@tweag.io>
2024-04-16 14:05:10 +0000

    wip

--- nix/apps/generate-index/default.nix
+++ nix/apps/generate-index/default.nix
@@ -6,5 +6,10 @@
 }:
 writers.makePythonWriter python312 python312Packages buildPackages.python312Packages
   "/bin/generate_index"
-  { flakeIgnore = [ "E501" ]; }
+  {
+    flakeIgnore = [
+      "E501" # line too long
+      "W503" # line break before binary operator
+    ];
+  }
   (builtins.readFile ./generate_index.py)
